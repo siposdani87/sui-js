037a8ae5ad45d00ac59c7ad763c86b6e3f1b8085
Daniel Sipos <dsipos@szintezis-net.hu>
2017-10-19 14:56:07 +0200

    Add position handling to Tooltip class

--- javascripts/module/tooltip.js
+++ javascripts/module/tooltip.js
@@ -8,10 +8,35 @@ goog.require('SUI');
  * @this {SUI.Tooltip}
  * @param {!SUI.Node} element
  * @param {string=} opt_message
+ * @param {string=} opt_position TOP|BOTTOM|LEFT|RIGHT
  */
-SUI.Tooltip = function(element, opt_message = '') {
+SUI.Tooltip = function (element, opt_message = '', opt_position = 'TOP') {
   this.element = element;
+  this._initOptions(opt_position);
   this._init(opt_message);
+};
+
+/**
+ * @private
+ * @param {string=} opt_position
+ * @returns {undefined}
+ */
+SUI.Tooltip.prototype._initOptions = function (opt_position = '') {
+  this.positionCssClass = 'mdl-tooltip--top';
+  switch (opt_position) {
+    case 'TOP':
+      this.positionCssClass = 'mdl-tooltip--top';
+      break;
+    case 'BOTTOM':
+      this.positionCssClass = 'mdl-tooltip--bottom';
+      break;
+    case 'LEFT':
+      this.positionCssClass = 'mdl-tooltip--left';
+      break;
+    case 'RIGHT':
+      this.positionCssClass = 'mdl-tooltip--right';
+      break;
+  }
 };
 
 /**
@@ -19,7 +44,7 @@ SUI.Tooltip = function(element, opt_message = '') {
  * @param {string=} opt_message
  * @returns {undefined}
  */
-SUI.Tooltip.prototype._init = function(opt_message = ''){
+SUI.Tooltip.prototype._init = function (opt_message = '') {
   this._createTooltip();
   var message = this._getMessage(opt_message);
   this._render(message);
@@ -29,11 +54,11 @@ SUI.Tooltip.prototype._init = function(opt_message = ''){
  * @param {string=} opt_message
  * @returns {string}
  */
-SUI.Tooltip.prototype._getMessage = function(opt_message = ''){
+SUI.Tooltip.prototype._getMessage = function (opt_message = '') {
   if (!opt_message) {
     opt_message = this.element.getAttribute('desc') || '';
     this.element.removeAttribute('desc');
-    if (opt_message){
+    if (opt_message) {
       this.tooltip.addClass('mdl-tooltip--large');
     }
     opt_message = this.element.getAttribute('title') || opt_message;
@@ -46,16 +71,16 @@ SUI.Tooltip.prototype._getMessage = function(opt_message = ''){
  * @private
  * @returns {undefined}
  */
-SUI.Tooltip.prototype._createTooltip = function(){
+SUI.Tooltip.prototype._createTooltip = function () {
   var id = this.element.getId();
-  if (SUI.isNull(id)){
+  if (SUI.isNull(id)) {
     id = SUI.generateId('tooltip');
     this.element.setId(id);
   }
 
   this.tooltip = new SUI.Node('span');
-  this.tooltip.addClass(['mdl-tooltip', 'mdl-tooltip--top']);
-  this.tooltip.setFor(/** @type {string} */ (id));
+  this.tooltip.addClass(['mdl-tooltip', this.positionCssClass]);
+  this.tooltip.setFor(/** @type {string} */(id));
   this.element.insertAfter(this.tooltip);
 };
 
@@ -64,7 +89,7 @@ SUI.Tooltip.prototype._createTooltip = function(){
  * @param {string} message
  * @returns {undefined}
  */
-SUI.Tooltip.prototype._render = function(message){
+SUI.Tooltip.prototype._render = function (message) {
   this.setMessage(message);
   SUI.mdl(this.tooltip);
 };
@@ -73,10 +98,10 @@ SUI.Tooltip.prototype._render = function(message){
  * @param {string=} opt_message
  * @returns {undefined}
  */
-SUI.Tooltip.prototype.setMessage = function(opt_message = ''){
-  if (opt_message){
+SUI.Tooltip.prototype.setMessage = function (opt_message = '') {
+  if (opt_message) {
     this.tooltip.removeStyle(['display']);
-    this.tooltip.setHtml(/** @type {string} */ (opt_message));
+    this.tooltip.setHtml(/** @type {string} */(opt_message));
   }
   else {
     this.tooltip.setStyle({
