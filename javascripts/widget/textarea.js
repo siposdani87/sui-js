8f4f2900ecc94ef9cad64a50ca821f43024b10fc
Daniel Sipos <siposdani87@gmail.com>
2019-06-23 23:20:21 +0200

    Create prototype of editable textarea

--- javascripts/widget/textarea.js
+++ javascripts/widget/textarea.js
@@ -29,13 +29,14 @@ SUI.widget.Textarea.prototype._init = function() {
   this.input.addEventListener('keyup', (input) => {
     const inputNode = input.getNode();
     this.modelChange(inputNode.value);
-    this.checkValidity();
+    this.checkValidity(true);
+    this.textboxNode.setHtml(inputNode.value);
   });
 
   this.input.addEventListener('change', (input) => {
     const inputNode = input.getNode();
     this.modelChange(inputNode.value);
-    this.checkValidity();
+    this.checkValidity(true);
   });
 };
 
@@ -45,11 +46,97 @@ SUI.widget.Textarea.prototype._init = function() {
  */
 SUI.widget.Textarea.prototype.render = function() {
   this.inputBlock.addClass(['mdl-textfield', 'mdl-js-textfield', 'mdl-textfield--floating-label']);
-  this.input.addClass(['mdl-textfield__input', 'mdl-textarea__input']);
+  this.input.addClass(['mdl-textfield__input', 'mdl-textarea__input', 'hidden']);
   if (this.label && this.label.exists()) {
     this.label.addClass('mdl-textfield__label');
   }
+
+  this.textboxNode = new SUI.Node('div');
+  this.textboxNode.addClass(['mdl-textfield__input', 'mdl-textarea__input', 'textbox']);
+  this.input.insertAfter(this.textboxNode);
+  this.oDoc = this.textboxNode.getNode();
+  this.oDoc.contentEditable = 'true';
+  this.textboxNode.addEventListener('keydown', (_node, event) => {
+    if (event.keyCode === 13) {
+      document.execCommand('defaultParagraphSeparator', false, 'p');
+    }
+  });
+  this.textboxNode.addEventListener('keyup', (node) => {
+    this.setValue(node.getHtml(true));
+  });
+
+  /*const toolbarNode = new SUI.Node('div');
+  toolbarNode.addClass('toolbar');
+  this.input.insertBefore(toolbarNode);
+
+  const boldButtonNode = new SUI.Node('a');
+  boldButtonNode.setAttribute('href', 'javascript:void(0)');
+  boldButtonNode.addClass('material-icons');
+  boldButtonNode.setHtml('format_bold');
+  boldButtonNode.addEventListener('click', () => {
+    this._formatDoc('bold');
+  });
+  toolbarNode.appendChild(boldButtonNode);
+
+  const codeButtonNode = new SUI.Node('a');
+  codeButtonNode.setAttribute('href', 'javascript:void(0)');
+  codeButtonNode.addClass('material-icons');
+  codeButtonNode.setHtml('code');
+  codeButtonNode.addEventListener('click', () => {
+    this._setHtmlMode(!this._isHtmlMode());
+  });
+  toolbarNode.appendChild(codeButtonNode);*/
+
   this.refresh();
+};
+
+/**
+ * @private
+ * @param {boolean} value
+ * @return {undefined}
+ */
+SUI.widget.Textarea.prototype._setHtmlMode = function(value) {
+  this.htmlMode = value;
+  this._setDocMode(this.htmlMode);
+};
+
+/**
+ * @private
+ * @return {boolean}
+ */
+SUI.widget.Textarea.prototype._isHtmlMode = function() {
+  return this.htmlMode === true;
+};
+
+/**
+ * @private
+ * @param {string} sCmd
+ * @param {*} sValue
+ * @return {undefined}
+ */
+SUI.widget.Textarea.prototype._formatDoc = function(sCmd, sValue) {
+  if (!this._isHtmlMode()) { 
+    this.oDoc.focus();
+    document.execCommand(sCmd, false, sValue);
+    this.setValue(this.oDoc.innerHTML);
+    this.oDoc.focus();
+  }
+};
+
+/**
+ * @private
+ * @param {boolean} _isHtmlMode
+ * @return {undefined}
+ */
+SUI.widget.Textarea.prototype._setDocMode = function(_isHtmlMode) {
+  if (_isHtmlMode) {
+    this.textboxNode.addClass('hidden');
+    this.input.removeClass('hidden');
+  } else {
+    this.textboxNode.removeClass('hidden');
+    this.input.addClass('hidden');
+  }
+  this.oDoc.focus();
 };
 
 /**
